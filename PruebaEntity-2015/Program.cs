08c3f653cc89e569dca77d86bcf00892f06a94b5
asder707 <abn.sebick@gmail.com>
2017-04-08 14:19:37 -0300

    Se creó un usuario desde console y se mostraron todos los usuarios

--- PruebaEntity-2015/Program.cs
+++ PruebaEntity-2015/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.Entity;
 
 namespace PruebaEntity_2015
 {
@@ -10,6 +11,33 @@ namespace PruebaEntity_2015
     {
         static void Main(string[] args)
         {
+
+            using (var db = new TO_DO_LISTEntities())
+            {
+                //crear y guardar nuevo usuario
+                Console.WriteLine("Ingrese el nombre");
+                string name = Console.ReadLine();
+                var user = new USER { name = name };
+                db.USER.Add(user);
+                db.SaveChanges();
+
+                //display all users
+
+                Console.WriteLine("Todos los usuarios en la base de datos");
+                var query = from u in db.USER
+                            orderby u.name descending
+                            select u;
+
+                foreach (var item in query)
+                {
+                    Console.WriteLine("ID: "+item.id+" nombre: "+item.name);
+                }
+
+                Console.ReadKey();
+
+            }
+                
+
         }
     }
 }
