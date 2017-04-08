aabb4c753f036bce68b6a67f8ff9c038746ad8c1
asder707 <abn.sebick@gmail.com>
2017-04-08 14:51:56 -0300

    se genero api de pruena para usuarios de objetos con xml

---
+++ ApiApp/Controllers/UserController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using ApiApp.Models;
+
+namespace ApiApp.Controllers
+{
+    public class UserController : ApiController
+    {
+        User[] users = new User[]
+        {
+            new User { Id = 1, Name = "Halfonso"},
+            new User { Id = 2, Name = "Halfons"},
+            new User { Id = 2, Name = "Laravel"}
+        };
+
+        public IEnumerable<User> GetAllUsers()
+        {
+            return users;
+        }
+
+        public IHttpActionResult GetUser(int id)
+        {
+            var user = users.FirstOrDefault((u) => u.Id == id);
+            if(user == null)
+            {
+                return NotFound();
+            }
+            return Ok(user);
+        }
+    }
+}
